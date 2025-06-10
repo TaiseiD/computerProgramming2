f3a56cf998512dfbf1768aa532f5b14acc9c4360
Мистер Викед <152064596+TaiseiD@users.noreply.github.com>
2025-06-10 17:17:10 +0800

    I created this using netbeans apache

---
+++ My Final Lab Act/Calculator Java/javaCalculator/src/main/java/javacalculator/javacalculator/JavaCalculator.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package javacalculator.javacalculator;
+
+/**
+ *
+ * @author IIseichi
+ */
+public class JavaCalculator {
+
+    public static void main(String[] args) {
+        new mainCalculator().setVisible(true);
+    }
+}
